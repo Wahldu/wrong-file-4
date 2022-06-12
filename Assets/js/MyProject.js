8170004bf0daaec4330e8337aafafaf886213411
Wahldu <107356966+Wahldu@users.noreply.github.com>
2022-06-12 17:01:30 +0200

    CV project

---
+++ Assets/js/MyProject.js
@@ -0,0 +1,21 @@
+function contact() {
+    var script_url = "https://script.google.com/macros/s/AKfycbz0FuV4gJPQUaKN4L7OdIUJfhbOce-p7_fGJlE7bKECTG8hkiDIrqDOvKYeBUugk4UK/exec";
+    //get data from form
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var subject = $("#subject").val();
+    var message = $("#message").val();
+    //send data
+    var url = script_url + "?callback=thanks&name=" + encodeURIComponent(name) + "&email=" + encodeURIComponent(email) + "&subject=" + encodeURIComponent(subject) + "&message=" + encodeURIComponent(message);
+    jQuery.ajax({
+        crossDomain: true,
+        url: url,
+        method: "GET",
+        dataType: "jsonp"
+    });
+}
+
+function thanks(e) {
+    //confirmation
+    alert("Thanks, I'll get back to you soon.");
+}
